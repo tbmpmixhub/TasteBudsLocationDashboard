b18720310fbf173afd1ef6b41f8c1eca97c69b8c
Evan Loria <evan.loria4@gmail.com>
2026-04-01 15:05:09 -0500

    Update start.sh with dynamic $PORT

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 # Start FastAPI token server on port 8000
 uvicorn token_server:app --host 0.0.0.0 --port 8000 &
 
-# Start Streamlit on port 8501
-streamlit run main.py --server.port 8501 --server.address 0.0.0.0
+# Start Streamlit on DO's assigned port
+streamlit run main.py --server.port $PORT --server.address 0.0.0.0
