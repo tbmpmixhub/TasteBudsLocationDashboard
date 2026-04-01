55c67f96c9bce51fb7714edc28d39d444982c2c4
Evan Loria <evan.loria4@gmail.com>
2026-04-01 16:58:10 -0500

    Update main.py, requrimeents.txt, and start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-# Start Streamlit on internal port 8501
-streamlit run main.py --server.port 8501 --server.address 0.0.0.0 &
-
-# Start FastAPI on the public port 8080
-uvicorn token_server:app --host 0.0.0.0 --port 8080
+streamlit run main.py --server.port $PORT --server.address 0.0.0.0
